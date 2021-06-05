1af1b11e8a686e6d21d76127947eac9a92f511b9
Adina Henderson <adinahenderson.mail@gmail.com>
2021-06-04 20:23:45 -0400

    Added comments to server.js file and modified code for random id

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
-const express = require("express")
-const path = require("path")
+// 1. Require 
+const express = require("express");
+const path = require("path");
 let db = require("./db/db.json");
-const fs = require("fs")
+const fs = require("fs");
 
-const app = express()
+// 2. Definition of app and PORT
+const app = express();
+// Creating port for Heroku or localhost
 let PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static('public'))
+// 3. Middleware needed for url, json and static express methods
+app.use(express.urlencoded({ extended: true })); // recognizes incoming request Object as strings or arrays
+app.use(express.json()); // recognizes incoming request Object as a JSON Object
+app.use(express.static('public')); 
 
+// 4. Send HTML notes to client
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
+// 5. Get json db response
 app.get("/api/notes", (req, res) => {
-    res.json(db)
+    res.json(db);
 })
 
+// 6. Add user notes to HTML body
 app.post("/api/notes", (req, res) => {
     let data = req.body;
-    data["id"] = Math.floor(Math.random()*20000000000)
-    db.push(data)
+    // Include an id to identify the user note, to be able to delete later
+    data["id"] = Math.floor(Math.random()* Date.now());
+    db.push(data);
     fs.writeFile("./db/db.json", JSON.stringify(db), err => {
         if(err) throw err
         res.json("dogs")
     })
 })
 
+// 7. Delete note
 app.delete("/api/notes/:id", (req, res) => {
     let IdToDelete = req.params.id;
     for(i=0;i<db.length;i++){
@@ -41,6 +50,7 @@ app.delete("/api/notes/:id", (req, res) => {
     })
 })
 
+// 8. Listen Server
 app.listen(PORT, function(){
     console.log(`working on port ${PORT}`)
 })
